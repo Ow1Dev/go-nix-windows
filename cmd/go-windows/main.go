ac7676182aabe2bcebf9373afa5fa3e73d15e309
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2024-10-31 18:35:03 +0000

    feat: add tray

--- cmd/go-windows/main.go
+++ cmd/go-windows/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+  "fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Window with system tray")
+
+
+  if desk, ok := a.(desktop.App); ok {
+    m := fyne.NewMenu("MyApp",
+      fyne.NewMenuItem("Show", func() {
+        w.Show()
+      }))
+    desk.SetSystemTrayMenu(m)
+  }
 
 	hello := widget.NewLabel("Hello Fyne!")
 	w.SetContent(container.NewVBox(
@@ -17,6 +28,9 @@ func main() {
 			hello.SetText("Welcome :)")
 		}),
 	))
+  w.SetCloseIntercept(func() {
+    w.Hide()
+  })
 
 	w.ShowAndRun()
 }
